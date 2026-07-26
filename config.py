42238a2e81f6d4ae2352a62a52c0a90a339564dc
d3khan <d3khan2.0@gmail.com>
2026-07-26 02:26:53 +0100

    assets limited

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     max_stake: float = 16
     min_payout: int = 92
     trade_duration: int = 30
-    max_consecutive_losses: int = 4
+    max_consecutive_losses: int = 3
     max_daily_loss: float = 50.0
     username: str = os.getenv("PO_USERNAME", "d3khan04")
     password: str = os.getenv("PO_PASSWORD", "fake_smile02")
@@ -27,25 +27,25 @@ class Settings(BaseSettings):
         "NZDJPY_otc", "CADJPY_otc", "CHFSGD_otc", "AUDJPY_otc", "AUDNZD_otc",
         "AUDNZD_otc", "EURGBP_otc",
 
-        # Crypto OTC
-        "BTCUSD_otc", "ETHUSD_otc", "LTCUSD_otc", "DOGEUSD_otc", "DOGE_otc",
-        "SOLUSD_otc", "TONUSD_otc", "ADAUSD_otc", "XRPUSD_otc", "BNBUSD_otc",
-        "TRXUSD_otc", "DOTUSD_otc", "LINKUSD_otc", "AVAXUSD_otc", "MATICUSD_otc",
-        "BCHUSD_otc", "XLMUSD_otc", "ATOMUSD_otc", "SHIBUSD_otc", "UNIUSD_otc",
+        # # Crypto OTC
+        # "BTCUSD_otc", "ETHUSD_otc", "LTCUSD_otc", "DOGEUSD_otc", "DOGE_otc",
+        # "SOLUSD_otc", "TONUSD_otc", "ADAUSD_otc", "XRPUSD_otc", "BNBUSD_otc",
+        # "TRXUSD_otc", "DOTUSD_otc", "LINKUSD_otc", "AVAXUSD_otc", "MATICUSD_otc",
+        # "BCHUSD_otc", "XLMUSD_otc", "ATOMUSD_otc", "SHIBUSD_otc", "UNIUSD_otc",
 
-        # Commodities & Indices
-        "XAUUSD_otc", "XAGUSD_otc", "USOIL_otc", "UKOIL_otc", "NAS100_otc",
-        "BRENT_otc", "WTICOIL_otc", "NGAS_otc", "PLATINUM_otc", "PALLADIUM_otc",
-        "DAX40_otc", "FTSE100_otc", "CAC40_otc", "JPN225_otc", "AUS200_otc",
-        "EUSTX50_otc", "HSI50_otc","US30_otc", "SPX500_otc",
+        # # Commodities & Indices
+        # "XAUUSD_otc", "XAGUSD_otc", "USOIL_otc", "UKOIL_otc", "NAS100_otc",
+        # "BRENT_otc", "WTICOIL_otc", "NGAS_otc", "PLATINUM_otc", "PALLADIUM_otc",
+        # "DAX40_otc", "FTSE100_otc", "CAC40_otc", "JPN225_otc", "AUS200_otc",
+        # "EUSTX50_otc", "HSI50_otc","US30_otc", "SPX500_otc",
 
-        # Stocks
-        "AAPL_otc", "AMZN_otc", "GOOGL_otc", "TSLA_otc", "MSFT_otc",
-        "AMD_otc", "PLTR_otc", "COIN_otc", "MARA_otc", "BABA_otc",
-        "INTC_otc", "IBM_otc", "ORCL_otc", "PYPL_otc", "V_otc",
-        "MA_otc", "BAC_otc", "JPM_otc", "WMT_otc", "DIS_otc",
-        "UBER_otc", "NKE_otc", "PEP_otc", "KO_otc", "XOM_otc",
-        "NVDA_otc", "META_otc", "NFLX_otc",
+        # # Stocks
+        # "AAPL_otc", "AMZN_otc", "GOOGL_otc", "TSLA_otc", "MSFT_otc",
+        # "AMD_otc", "PLTR_otc", "COIN_otc", "MARA_otc", "BABA_otc",
+        # "INTC_otc", "IBM_otc", "ORCL_otc", "PYPL_otc", "V_otc",
+        # "MA_otc", "BAC_otc", "JPM_otc", "WMT_otc", "DIS_otc",
+        # "UBER_otc", "NKE_otc", "PEP_otc", "KO_otc", "XOM_otc",
+        # "NVDA_otc", "META_otc", "NFLX_otc",
     ]
 
     model_config = ConfigDict(
